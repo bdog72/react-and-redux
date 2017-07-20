ac84b8052aca436d546467770e1806c34190ac2c
Brian Bycynski <high5bri@hotmail.com>
2017-07-20 06:13:52 -0400

    added some reducer functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
-
 class App extends Component {
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <h1>What up Dog</h1>
     </div>
   }
 }
---
+++ src/reducers.js
@@ -0,0 +1,72 @@
+
+const initialUsersState = {
+  lastUpdated: 0,
+  info: {
+    name: '',
+    uid: '',
+    avatar: ''
+  }
+}
+
+function user (state = initialUsersState, action) {
+  switch (action.type) {
+    case FETCHING_USER_SUCCESS:
+      return {
+        ...state,
+        info: action.user,
+        lastUpdated: action.timestamp
+      }
+    default:
+      return state
+  }
+}
+
+const initialState = {
+  isFetching: false,
+  error: '',
+  isAuthed: false,
+  authedId: ''
+}
+
+function users (state = initialState, action) {
+  switch (action.type) {
+    case AUTH_USER:
+      return {
+        ...state,
+        isAuthed: true,
+        authedId: action.uid
+      }
+    case UNAUTH_USER:
+      return {
+        ...state,
+        isAuthed: false,
+        authed: ''
+      }
+    case FETCHING_USER:
+      return {
+        ...state,
+        isFetching: true
+      }
+    case FETCHING_USER_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    case FETCHING_USER_SUCCESS:
+      return action.user === null
+        ? {
+          ...state,
+          error: '',
+          isFetching: false
+        }
+        : {
+          ...state,
+          isFetching: false,
+          error: '',
+          [action.uid]: user(state[action.uid], action)
+        }
+    default:
+      return state
+  }
+}
